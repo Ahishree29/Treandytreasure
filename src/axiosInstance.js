f8babaacd7e2f96e0a9cba6856ffd0dc046240f8
Ahishree Rai P <ahishree29@gmail.com>
2024-04-02 13:20:05 +0530

    fix

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -3,25 +3,25 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: "https://treandytreasure.onrender.com",
   // baseURL: "http://127.0.0.1:5000",
-  timeout: 5000,
-  headers: {
-    "Content-Type": "application/json",
-  },
+  // timeout: 5000,
+  // headers: {
+  //   "Content-Type": "application/json",
+  // },
 });
 
-instance.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("userInfo")
-      ? JSON.parse(localStorage.getItem("userInfo")).token
-      : null;
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
+// instance.interceptors.request.use(
+//   (config) => {
+//     const token = localStorage.getItem("userInfo")
+//       ? JSON.parse(localStorage.getItem("userInfo")).token
+//       : null;
+//     if (token) {
+//       config.headers.Authorization = `Bearer ${token}`;
+//     }
+//     return config;
+//   },
+//   (error) => {
+//     return Promise.reject(error);
+//   }
+// );
 
 export default instance;
