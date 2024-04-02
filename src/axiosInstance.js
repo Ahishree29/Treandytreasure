e95dbe02a229101fdeb6455c404cdeedd33d6fbe
Ahishree Rai P <ahishree29@gmail.com>
2024-04-02 08:24:41 +0530

    baseURL fix

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  // baseURL: "https://treandytreasure.onrender.com",
-  baseURL: "http://127.0.0.1:5000",
+  baseURL: "https://treandytreasure.onrender.com",
+  // baseURL: "http://127.0.0.1:5000",
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
