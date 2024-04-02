a2afef92813c4237950286b3f2cf78d459dce94b
Ahishree Rai P <ahishree29@gmail.com>
2024-04-02 15:06:04 +0530

    bearer

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,33 +1,18 @@
 import axios from "axios";
+
 const getToken = () => {
   const token = localStorage.getItem("token");
   return token ? token : null;
 };
+
 const instance = axios.create({
   baseURL: "https://treandytreasure.onrender.com",
   // baseURL: "http://127.0.0.1:5000",
 
   headers: {
     "Content-Type": "application/json",
-    Authorization: getToken(),
+    Authorization: `Bearer ${getToken()}`,
   },
 });
-
-// instance.interceptors.request.use(
-//   (config) => {
-//     console.log("Interceptor called");
-//     const token = localStorage.getItem("userInfo")
-//       ? JSON.parse(localStorage.getItem("userInfo")).token
-//       : null;
-//     if (token) {
-//       console.log("Token:", token);
-//       config.headers.Authorization = `Bearer ${token}`;
-//     }
-//     return config;
-//   },
-//   (error) => {
-//     return Promise.reject(error);
-//   }
-// );
 
 export default instance;
