3d2517c09c20f9601423cc95f3a9216840b1f2b2
Ahishree Rai P <ahishree29@gmail.com>
2024-04-02 15:35:00 +0530

    JWT correction

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -3,14 +3,14 @@ const User = require("../models/userModel");
 const asyncHandler = require("express-async-handler");
 const protect = asyncHandler(async (req, res, next) => {
   let token;
-  console.log("hello", req.headers.authorization);
+
   if (
     req.headers.authorization &&
     req.headers.authorization.startsWith("Bearer")
   ) {
     try {
       token = req.headers.authorization.split(" ")[1];
-      const decoded = jwt.verify(token, process.env.JWT_Secret);
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
       req.user = await User.findById(decoded.id).select("-password");
       next();
     } catch (error) {
